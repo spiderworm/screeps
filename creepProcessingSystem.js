14ab362b762fe45da6e0f61021fd908c802e07cd
David Millet <david.millet@gmail.com>
2016-02-28 15:56:02 -0800

    update

--- creepProcessingSystem.js
+++ creepProcessingSystem.js
@@ -2,15 +2,26 @@
 var composer = require('composer');
 var creepUtil = require('creepUtil');
 var creepRoleSystem = require('creepRoleSystem');
+var creepBoredomUtil = require('creepBoredomUtil');
 
 var creepProcessingSystem = {
-    tick: function() {
-    	creepUtil.forEach(function(creep) {
-    		var role = creepRoleSystem.getCreepRole(creep);
-    		var tick = creepRoleSystem.getTickFunction(role);
-    		tick(creep);
-    	});
-    }
+
+	tick: function() {
+
+		creepUtil.forEach(function(creep) {
+			var role = creepRoleSystem.getCreepRole(creep);
+			var tick = creepRoleSystem.getTickFunction(role);
+			tick(creep);
+			if (creepBoredomUtil.getBoredom(creep) > 10) {
+				if (creepRoleSystem.creepCanHaveRole(creep, creepRoleSystem.roles.harvester)) {
+					creepRoleSystem.assignRole(creep, creepRoleSystem.roles.harvester);
+					creepBoredomUtil.removeBoredom(creep);
+				}
+			}
+		});
+
+	}
+
 };
 
 composer.addFeature(creepProcessingSystem, 'memory', 'creepProcessingSystem');
--- creepRoleSystem.js
+++ creepRoleSystem.js
@@ -16,5 +16,27 @@ CreepRoleSystem.prototype.getCreepRole = function(creep) {
 CreepRoleSystem.prototype.getTickFunction = function(role) {
 	return role.tick;
 };
-
+CreepRoleSystem.prototype.assignRole = function(creep, role) {
+	creep.memory.role = role.name;
+};
+CreepRoleSystem.prototype.creepCanHaveRole = function(creep, role) {
+	return this.creepHasRole(creep, role) || this.creepHasParts(creep, role);
+};
+CreepRoleSystem.prototype.creepHasRole = function(creep, role) {
+	return creep.memory.role === role.name;
+};
+CreepRoleSystem.prototype.creepHasParts = function(creep, role) {
+	var neededParts = [].concat(role.body);
+	creep.body.forEach(function(part) {
+		if (part.hits > 0) {
+			var i = neededParts.indexOf(part.type);
+			if (i !== -1) {
+				neededParts.splice(i,1);
+			}
+		}
+	});
+	var result = neededParts.length === 0;
+	return result;
+};
+    
 module.exports = new CreepRoleSystem();
