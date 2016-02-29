c992ff34bb701b8570309ece21e270835c4ea691
David Millet <david.millet@gmail.com>
2016-02-29 08:49:17 -0800

    update

--- creepProcessingSystem.js
+++ creepProcessingSystem.js
@@ -8,20 +8,14 @@ var economicSystem = require('economicSystem');
 var creepProcessingSystem = {
 
 	tick: function() {
-
 		creepUtil.forEach(function(creep) {
 			var role = creepRoleSystem.getCreepRole(creep);
 			var tick = creepRoleSystem.getTickFunction(role);
 			tick(creep);
 			if (creepBoredomUtil.getBoredom(creep) > 10) {
-				if (economicSystem.giveRole(creep))
-				if (creepRoleSystem.creepCanHaveRole(creep, creepRoleSystem.roles.harvester)) {
-					creepRoleSystem.assignRole(creep, creepRoleSystem.roles.harvester);
-					creepBoredomUtil.removeBoredom(creep);
-				}
+				this.findRole(creep);
 			}
-		});
-
+		}.bind(this));
 	},
 
 	findRole: function(creep) {
